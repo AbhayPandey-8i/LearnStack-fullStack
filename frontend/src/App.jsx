f4e290e09f2bd13cf729a2e5910db18bbe227989
Abhay Pandey <pandeyabhay055@gmail.com>
2026-04-16 00:54:56 +0530

    updated code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
+import Login from './pages/Login.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
+     
       <h1 className='text-black'>Hello</h1>
+      <Login/>
       
     </div>
   )
