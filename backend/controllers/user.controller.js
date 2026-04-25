7e04d2814700a24c57f53f06d4625c8670287750
Abhay Pandey <pandeyabhay055@gmail.com>
2026-04-25 22:05:07 +0530

    add login controllers

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -41,3 +41,35 @@ export const register = async (req, res) => {
 }
 
 //login
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body
+
+        if (!email || !password) {
+            return res.status(400).json({
+                success: false,
+                messsage: "All fields are required"
+            })
+        }
+
+        const user = await User.findOne({ email })
+        if (!user) {
+            return res.status(400).json({
+                success: false,
+                message: "Incorrect email or password"
+            })
+        }
+
+        const isPasswordMatch = await bcrypt.compare(password, user.password)
+
+        if (!isPasswordMatch) {
+            return res.status(400).json({
+                success:false,
+                message:"Incorrect email or password"
+            })
+        }
+
+    } catch (error) {
+        console.log(error)
+    }
+}
