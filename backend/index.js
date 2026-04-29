c6a0340488e60b4db19911eb06c4ad8746b0fdaf
Abhay Pandey <pandeyabhay055@gmail.com>
2026-04-29 23:07:55 +0530

    add api

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import connectDB from "./database/dbConnect.js";
+import userRoute from "./routes/user.routes.js"
 
 dotenv.config({});
 connectDB()
@@ -8,6 +9,9 @@ connectDB()
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+//api
+app.use("/api/v1/user", userRoute)
 
 app.listen(PORT, () => {
   console.log(`Server running at port ${PORT}`);
