1f1a329512e3cc95b93218c35a916c09a579606f
Abhay Pandey <pandeyabhay055@gmail.com>
2026-05-02 21:59:35 +0530

    update code

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ import express from "express"
 import dotenv from "dotenv"
 import connectDB from "./database/dbConnect.js";
 import userRoute from "./routes/user.routes.js"
+import cookieParser from "cookie-parser";
+import cors from "cors"
 
 dotenv.config({});
 connectDB()
@@ -9,6 +11,11 @@ connectDB()
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+//middlewares
+app.use(express.json())
+app.use(cookieParser)
+app.use(cors())
 
 //api
 app.use("/api/v1/user", userRoute)
