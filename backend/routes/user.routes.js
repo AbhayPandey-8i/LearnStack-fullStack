9962fdf9bd086f52675255ec1867ca293249f571
Abhay Pandey <pandeyabhay055@gmail.com>
2026-05-04 23:54:27 +0530

    fix bugs

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -3,7 +3,7 @@ import { login, register } from "../controllers/user.controller.js"
 
 const router = express.Router();
 
-router.route("/register").post(register)
+router.route("/register").post(register);
 router.route("/login").post(login)
 
-export default router
+export default router;
